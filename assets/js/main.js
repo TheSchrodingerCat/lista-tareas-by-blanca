504eb45133b1b4ad06aa3b3bd4024bb8e01af474
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-02 14:05:56 -0300

    no entiendo nada

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,50 @@ var tareas = arr.slice(0,10);
 var miUl = document.getElementById("tareas");
 
 var liAux, textAux;
-tareas.forEach(function(element){
-	liAux = document.createElement("li"); //crea el elemento li
-	textAux = document.createTextNode(element.title); //crea el texto de la etiqueta li
-	liAux.appendChild(textAux); //deja el texto dentro del li, pero no del ul
-	miUl.appendChild(liAux); //deja el li dentro del ul
-});
+tareas.forEach(renderTask);
 
-function addTask(){
-	var miInput = document.getElementById("task").value;
+function renderTask(task){
 	liAux = document.createElement("li");
-	textAux = document.createTextNode(miInput);
+	textAux = document.createTextNode(task.title);
+
+	var enlace = document.createElement("a");
+	var textEdit = document.createTextNode(" Editar");
+
 	liAux.appendChild(textAux);
 	miUl.appendChild(liAux);
-	document.getElementById("task").value = "";
+	enlace.appendChild(textEdit);
+	enlace.setAttribute("onclick", "editTask(this, " + task.id + ")");
+	liAux.appendChild(enlace);
 }
+
+function editTask(enlace,idTask){
+	console.log(enlace);
+	console.log(idTask);
+	var nuevoTexto = prompt("Ingrese el nuevo texto: ");
+	var tarea = tareas.filter(function(element){
+		return element.id == idtask;
+	});
+	tarea[0].title = nuevoTexto;
+	var miLi = enlace.parentNode;
+	miLi.innerHTML = nuevoTexto + enlace;
+}
+
+function Task(title){
+	this.title = title;
+	this.userId = Math.floor((Math.random() * 10) + 1);
+	this.completed = false;
+	this.id = tareas.length + 1;
+}
+
+function addTask(){
+	var miInput = document.getElementById("task");
+
+	if(miInput.value != ""){
+		var task = new Task(miInput.value);
+		tareas.push(task);
+		renderTask(task);
+		miInput.value = "";
+	}
+	
+}
+
